032e65573733e7e4d086c433f24a1deaafb1fc4f
unknown989 <unknown989@protonmail.com>
2020-12-08 05:06:01 +0000

    Adding Redirection

--- main.py
+++ main.py
@@ -13,10 +13,16 @@ class Server():
 
         host = sc["HOST"]
         port = sc["PORT"]
+        self.redirect = sc["REDIRECT"]
+        self.redirect = True if self.redirect == "true" else False
+        if self.redirect:
+            self.redirect_page = sc["REDIRECT_PAGE"]
+        else:
+            pass
         self.mainFile = sc["INDEXFILE"]
         self.defDir = sc["DEFAULTDIR"]
         self.ip,self.cl = [],[]
-
+        
         self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         print("Binding...")
         self.s.bind((host,int(port)))
@@ -25,6 +31,7 @@ class Server():
         print("Starting The Thread For Accepting Connections...")
         snt(self.accept())
         print("Done")
+        self.s.close()
     def accept(self):
         while True:
             i,c = self.s.accept()
@@ -55,7 +62,16 @@ class Server():
                     for cc in content:
                         self.sendMsg(cc.encode(),i,enc=False)
             except:
-                self.sendMsg(str("404 Not Found"),i)
+                if not self.redirect:
+                    self.sendMsg(str("HTTP/1.0 404 Not Found\r\n\r\n"),i)
+                else:
+                    #if not filename.find("favicon.ico"):
+                    self.sendMsg("HTTP/1.0 200 OK\r\n",i)
+                    self.sendMsg("Content-Type: text/html\r\n\r\n",i)
+                    #self.sendMsg(str("Location: {redirect_page}\r\n").format(redirect_page="/"+self.redirect_page if not self.redirect_page.startswith("/") else self.redirect_page),i)
+                    self.sendMsg(str("<html><body><script type='text/javascript'>window.location = '{}'</script></body></html>").format("/"+self.redirect_page if not self.redirect_page.startswith("/") else self.redirect_page),i)
+                    #else:
+                    #    self.sendMsg("HTTP/1.0 404 Not Found\r\n\r\n",i)
             i.close()
     
     def sendMsg(self,msg,ip,enc=True):
